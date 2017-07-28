1e43e4d1ef8b1c5beb789fef55e3ea31aa6c101d
juantomaylla <juan.tomaylla@gmail.com>
2017-07-28 18:28:19 -0500

    1st commit

---
+++ Models/Movie.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace MvcMovie.Models
+{
+	//public class Movie
+	//{
+	//	public int ID { get; set; }
+	//	public string Title { get; set; }
+	//	[DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+	//	[Display(Name = "Release Date")]
+	//	[DataType(DataType.Date)]
+	//	public DateTime ReleaseDate { get; set; }
+	//	public string Genre { get; set; }
+	//	public decimal Price { get; set; }
+ //       public string Rating { get; set; }
+	//}
+
+	public class Movie
+	{
+		public int ID { get; set; }
+
+		[StringLength(60, MinimumLength = 3)]
+		[Required]
+		public string Title { get; set; }
+
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+		[Display(Name = "Release Date")]
+		[DataType(DataType.Date)]
+		public DateTime ReleaseDate { get; set; }
+
+		[Range(1, 100)]
+		[DataType(DataType.Currency)]
+		public decimal Price { get; set; }
+
+		[RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+		[Required]
+		[StringLength(30)]
+		public string Genre { get; set; }
+
+		//[RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$")]
+		//[StringLength(5)]
+		//[Required]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z''-'\s]*$"),Required,StringLength(5)]
+		public string Rating { get; set; }
+	}
+ }
